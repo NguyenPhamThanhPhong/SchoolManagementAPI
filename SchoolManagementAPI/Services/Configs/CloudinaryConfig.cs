7842c9adda161f6353015519c5f7069f5e675f84
Phong <thanhphong17160@gmail.com>
2023-12-23 23:49:37 -0800

    save for backup

--- SchoolManagementAPI/Services/Configs/CloudinaryConfig.cs
+++ SchoolManagementAPI/Services/Configs/CloudinaryConfig.cs
@@ -2,5 +2,13 @@
 {
     public class CloudinaryConfig
     {
+#pragma warning disable CS8618
+
+        public string CloudName { get; set; }
+        public string APIKey { get; set; }
+        public string APISecretKey { get; set; }
+        public string UserFolderName { get; set; }
+        public string PostFolderName { get; set; }
+        public string MessageFolderName { get; set; }
     }
 }
