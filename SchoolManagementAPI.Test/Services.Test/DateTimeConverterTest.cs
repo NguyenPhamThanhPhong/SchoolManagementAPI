99919774cdde5eedfbc0eb5b4a634b4e8b57f1e7
nhvtmanh <nhvtmanh@gmail.com>
2024-01-09 22:22:49 +0700

    add more test cases to validate DateTimeConverter

--- SchoolManagementAPI.Test/Services.Test/DateTimeConverterTest.cs
+++ SchoolManagementAPI.Test/Services.Test/DateTimeConverterTest.cs
@@ -27,5 +27,70 @@ namespace SchoolManagementAPI.Test.Services.Test
             // Assert
             Assert.AreEqual(new DateTime(2024, 1, 1), result);
         }
+
+        [Test]
+        public void Read_ShouldReturnCorrectDateTime_WithDifferentFormat()
+        {
+            // Arrange
+            var converter = new DateTimeConverter("dd/MM/yyyy");
+            var jsonString = "\"01/01/2024\"";
+            var jsonBytes = Encoding.UTF8.GetBytes(jsonString);
+            var reader = new Utf8JsonReader(jsonBytes);
+
+            // Act
+            reader.Read();
+            var result = converter.Read(ref reader, typeof(DateTime), null);
+
+            // Assert
+            Assert.AreEqual(new DateTime(2024, 1, 1), result);
+        }
+
+        [Test]
+        public void Read_ShouldThrowException_WhenInvalidDate()
+        {
+            // Arrange
+            var converter = new DateTimeConverter("yyyy-MM-dd");
+            var jsonString = "\"2024-13-01\""; // Invalid date
+            var jsonBytes = Encoding.UTF8.GetBytes(jsonString);
+            var reader = new Utf8JsonReader(jsonBytes);
+            reader.Read();
+
+            // Act & Assert
+            JsonException exception = null;
+            try
+            {
+                var result = converter.Read(ref reader, typeof(DateTime), null);
+            }
+            catch (JsonException ex)
+            {
+                exception = ex;
+            }
+
+            Assert.IsNotNull(exception);
+        }
+
+        [Test]
+        public void Read_ShouldThrowException_WhenInvalidFormat()
+        {
+            // Arrange
+            var converter = new DateTimeConverter("yyyy-MM-dd");
+            var jsonString = "\"01/01/2024\""; // Date doesn't match the format
+            var jsonBytes = Encoding.UTF8.GetBytes(jsonString);
+            var reader = new Utf8JsonReader(jsonBytes);
+            reader.Read();
+
+            // Act & Assert
+            JsonException exception = null;
+            try
+            {
+                var result = converter.Read(ref reader, typeof(DateTime), null);
+            }
+            catch (JsonException ex)
+            {
+                exception = ex;
+            }
+
+            Assert.IsNotNull(exception);
+        }
     }
 }
